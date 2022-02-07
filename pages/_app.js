918a1515862d128a81532ec60264d0cef8c0e9c0
FrenchGrandpa <pepijn.pinckers@hotmail.nl>
2022-02-07 21:45:32 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import 'antd/dist/antd.css'
-import '../styles/vars.css'
-import '../styles/global.css'
+// import 'antd/dist/antd.css'
+// import '../styles/vars.css'
+// import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
